9f19ffbc6ff5d483e04ff7779daa9b8fc20db49d
rangel3l2 <rangel.silva@estudante.ifms.edu.br>
2022-12-09 15:36:32 -0400

    update import

--- src/main/java/ifms/com/div/divisao/controller/DivisaoController.java
+++ src/main/java/ifms/com/div/divisao/controller/DivisaoController.java
@@ -7,7 +7,7 @@ import ifms.com.div.divisao.model.DivisaoModel;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @RestController
